ceae129129f23eb84707801e0a30bc67ed91f502
maxmccann <mmaxmmccann@gmail.com>
2018-03-03 13:21:15 -0500

    Add unicode support

--- updateCardDatabase.py
+++ updateCardDatabase.py
@@ -2,11 +2,20 @@
 #JSON parsing module
 import json
 #for sorting the cards
-from operator import itemgetter, attrgetter, methodcaller
 #for reading the now more complicated TSV
-import csv
 #for interacting with the Database
 import pyodbc
+#For dealing with unicode from the json
+import codecs
+# For dealing with unicode from the json
+import codecs
+import json
+# for sorting the cards
+# for reading the now more complicated TSV
+# for interacting with the Database
+import pyodbc
+
+
 #Classes
 #Class for a card
 class mtgcard:
@@ -17,12 +26,12 @@ class mtgcard:
   def __str__(self):
     return '\t'.join([self.name, self.cid, self.set]) + '\n'
 #load the set list
-JSON = open('AllSets.json','r')
-cardsJSON = json.load(JSON)
+cardsJSON = json.load(codecs.open('./AllCards.json', 'r', 'utf-8-sig'))
 #open connection to database
 mtgdb = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       "Server=DESKTOP-62G1QJF;"
                       "Database=MTG;"
                       "Trusted_Connection=yes;")
 #Loop through all the cards and add them to the database
-for card in cardsJSON[eaSet]['cards']:
+for card in cardsJSON:
+    print(card)
